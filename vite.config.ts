11e5a7812fb4724ecb22555a73dc2ccdb963b8e3
[dyad] <anselmo.cstecnologia@gmail.com>
2026-07-15 17:35:04 -0300

    [dyad] Corrigir falha crítica de inicialização do dev server

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-import { nitro } from "nitro/vite";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -13,7 +11,7 @@ export default defineConfig({
       overlay: false,
     },
   },
-  plugins: [react(), nitro()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
