bef71c4e7c50091f09dd3fb3c4fda5b5c8ce37dd
[dyad] <anselmo.cstecnologia@gmail.com>
2026-07-15 17:41:05 -0300

    [dyad] Corrigir falha ao chamar Edge Function

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,9 +2,4 @@ import { defineConfig } from "nitro";
 
 export default defineConfig({
   serverDir: "./server",
-  runtimeConfig: {
-    n8nCadastroUrl: "",
-    n8nCancelamentoUrl: "",
-    n8nApiKey: "",
-  },
 });
